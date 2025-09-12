d4959b9db415dcedc632f4a9734940f1ff122e9f
keyjin88 <keyjin88@gmail.com>
2025-09-12 08:31:47 +0300

    Complete 9.16HW

---
+++ 3-bin/bin.go
@@ -0,0 +1,25 @@
+package main
+
+import "time"
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+type BinList struct {
+	bins []Bin
+}
+
+func (b *Bin) NewBin(id string, private bool, createdAt time.Time, name string) {
+	b.id = id
+	b.private = private
+	b.createdAt = createdAt
+	b.name = name
+}
+
+func (b *BinList) NewBinList(bins []Bin) {
+	b.bins = bins
+}
